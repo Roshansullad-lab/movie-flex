5a818c494a68a5a732d98f814572023755b28043
sullad855 <sullad855@gmail.com>
2026-03-28 03:11:42 +0530

    removed localhost to run on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//export default defineConfig({
-//    plugins: [react()],
-//    root: ".", // ensures Vite looks in project root for index.html
-//    server: { port: 3000, host: "localhost" }
-//});
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     root: ".", // ensures Vite looks in project root for index.html
+    server: { port: 3000 }
+});
 
-    server: {
-        port: 3000, // local dev port
-    },
-    build: {
-        outDir: 'dist', // Vercel expects dist folder
-    },
-    base: '/', // important: ensures assets resolve correctly on Vercel
-})
+// https://vitejs.dev/config/
+//export default defineConfig({
+//    plugins: [react()],
+//    root: ".", // ensures Vite looks in project root for index.html
+
+//    server: {
+//        port: 3000, // local dev port
+//    },
+//    build: {
+//        outDir: 'dist', // Vercel expects dist folder
+//    },
+//    base: '/', // important: ensures assets resolve correctly on Vercel
+//})
 
