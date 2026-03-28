0ff22fbb7c1198ffa79570610f971fc67cd35f22
sullad855 <sullad855@gmail.com>
2026-03-28 12:45:39 +0530

    test change3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//export default defineConfig({
-//    plugins: [react()],
-//    root: ".", // ensures Vite looks in project root for index.html
-//    server: { port: 3000 }
-//});
 export default defineConfig({
     plugins: [react()],
-    build: {
-        outDir: 'dist', // Vercel expects dist folder
-    },
-    "routes": [
-        { "src": "/(.*)", "dest": "/index.html" }
-    ],
-    base: '/', // ensures assets resolve correctly
-})
+    root: ".", // ensures Vite looks in project root for index.html
+    server: { port: 3000 }
+});
+//export default defineConfig({
+//    plugins: [react()],
+//    build: {
+//        outDir: 'dist', // Vercel expects dist folder
+//    },
+//    "routes": [
+//        { "src": "/(.*)", "dest": "/index.html" }
+//    ],
+//    base: '/', // ensures assets resolve correctly
+//})
 // https://vitejs.dev/config/
 //export default defineConfig({
 //    plugins: [react()],
