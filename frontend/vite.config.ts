301ab334f1f5b8d014ed43a186503ac4f7f9eeb9
sullad855 <sullad855@gmail.com>
2026-03-28 13:14:19 +0530

    2test change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-//export default defineConfig({
-//    plugins: [react()],
-//    root: ".", // ensures Vite looks in project root for index.html
-//    server: { port: 3000 }
-//});
-
-
 export default defineConfig({
     plugins: [react()],
-    build: {
-        outDir: 'dist'
-    },
-    base: '/'
+    root: ".", // ensures Vite looks in project root for index.html
+    server: { port: 3000 }
 });
+
+
+//export default defineConfig({
+//    plugins: [react()],
+//    build: {
+//        outDir: 'dist'
+//    },
+//    base: '/'
+//});
 //export default defineConfig({
 //    plugins: [react()],
 //    build: {
