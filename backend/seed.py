d53360266e0b838264bf4a4afb11601f41dbb886
sullad855 <sullad855@gmail.com>
2026-03-28 14:07:28 +0530

    updated the pics path in the database

--- backend/seed.py
+++ backend/seed.py
@@ -8,23 +8,23 @@ Base.metadata.create_all(bind=engine)
 db = SessionLocal()
 
 # Directors
-hirani = models.Director(name="Rajkumar Hirani", birth_year=1962, bio="Known for socially relevant films.",poster_url="/public/pics/rj.jpg")
-tiwari = models.Director(name="Nitesh Tiwari", birth_year=1973, bio="Indian film director and screenwriter.",poster_url="/public/pics/nt.jpg")
-akhtar = models.Director(name="Zoya Akhtar", birth_year=1972, bio="Indian filmmaker known for contemporary dramas.",poster_url="/public/pics/za.jpg")
-sippy = models.Director(name="Ramesh Sippy", birth_year=1947, bio="Director of the classic Sholay.",poster_url="/public/pics/rs.jpg")
-johar = models.Director(name="Karan Johar", birth_year=1972, bio="Popular Bollywood director and producer.",poster_url="/public/pics/kz.jpg")
+hirani = models.Director(name="Rajkumar Hirani", birth_year=1962, bio="Known for socially relevant films.",poster_url="/pics/rj.jpg")
+tiwari = models.Director(name="Nitesh Tiwari", birth_year=1973, bio="Indian film director and screenwriter.",poster_url="/pics/nt.jpg")
+akhtar = models.Director(name="Zoya Akhtar", birth_year=1972, bio="Indian filmmaker known for contemporary dramas.",poster_url="/pics/za.jpg")
+sippy = models.Director(name="Ramesh Sippy", birth_year=1947, bio="Director of the classic Sholay.",poster_url="/pics/rs.jpg")
+johar = models.Director(name="Karan Johar", birth_year=1972, bio="Popular Bollywood director and producer.",poster_url="/pics/kz.jpg")
 
 db.add_all([hirani, tiwari, akhtar, sippy, johar])
 db.commit()
 
 # Actors
-aamir = models.Actor(name="Aamir Khan", birth_year=1965, bio="Indian actor, director, and producer.",poster_url="/public/pics/am.jpg")
-kareena = models.Actor(name="Kareena Kapoor", birth_year=1980, bio="Bollywood actress known for versatile roles.",poster_url="/public/pics/kr.jpg")
-ranveer = models.Actor(name="Ranveer Singh", birth_year=1985, bio="Energetic Bollywood actor.",poster_url="/public/pics/rn.jpg")
-alia = models.Actor(name="Alia Bhatt", birth_year=1993, bio="Actress known for modern Bollywood hits.",poster_url="/public/pics/al.jpg")
-amitabh = models.Actor(name="Amitabh Bachchan", birth_year=1942, bio="Legendary Bollywood actor.",poster_url="/public/pics/amt.jpg")
-shahrukh = models.Actor(name="Shah Rukh Khan", birth_year=1965, bio="Bollywood superstar.",poster_url="/public/pics/shk.jpg")
-kajol = models.Actor(name="Kajol", birth_year=1974, bio="Actress known for iconic romantic films.",poster_url="/public/pics/kjl.jpg")
+aamir = models.Actor(name="Aamir Khan", birth_year=1965, bio="Indian actor, director, and producer.",poster_url="/pics/am.jpg")
+kareena = models.Actor(name="Kareena Kapoor", birth_year=1980, bio="Bollywood actress known for versatile roles.",poster_url="/pics/kr.jpg")
+ranveer = models.Actor(name="Ranveer Singh", birth_year=1985, bio="Energetic Bollywood actor.",poster_url="/pics/rn.jpg")
+alia = models.Actor(name="Alia Bhatt", birth_year=1993, bio="Actress known for modern Bollywood hits.",poster_url="/pics/al.jpg")
+amitabh = models.Actor(name="Amitabh Bachchan", birth_year=1942, bio="Legendary Bollywood actor.",poster_url="/pics/amt.jpg")
+shahrukh = models.Actor(name="Shah Rukh Khan", birth_year=1965, bio="Bollywood superstar.",poster_url="/pics/shk.jpg")
+kajol = models.Actor(name="Kajol", birth_year=1974, bio="Actress known for iconic romantic films.",poster_url="/pics/kjl.jpg")
 
 db.add_all([aamir, kareena, ranveer, alia, amitabh,shahrukh,kajol])
 db.commit()
@@ -47,7 +47,7 @@ three_idiots = models.Movie(
     release_year=2009,
     description="Comedy-drama about friendship and education.",
     director=hirani,
-    poster_url="/public/pics/3.jpg",
+    poster_url="/pics/3.jpg",
     genres=[drama, comedy],
     actors=[aamir, kareena]
 )
@@ -57,7 +57,7 @@ dangal = models.Movie(
     release_year=2016,
     description="Biographical sports drama about wrestler Mahavir Singh Phogat.",
     director=tiwari,
-    poster_url="/public/pics/dang.jpg",
+    poster_url="/pics/dang.jpg",
     genres=[drama, action],
     actors=[aamir]
 )
@@ -67,7 +67,7 @@ gully_boy = models.Movie(
     release_year=2019,
     description="Musical drama inspired by Mumbai street rappers.",
     director=akhtar,
-    poster_url="/public/pics/gul.jpg",
+    poster_url="/pics/gul.jpg",
     genres=[drama, musical],
     actors=[ranveer, alia]
 )
@@ -77,7 +77,7 @@ sholay = models.Movie(
     release_year=1975,
     description="Classic action-adventure film.",
     director=sippy,
-    poster_url="/public/pics/sho.jpg",
+    poster_url="/pics/sho.jpg",
     genres=[action, drama],
     actors=[amitabh]
 )
@@ -87,7 +87,7 @@ kkkg = models.Movie(
     release_year=2001,
     description="Family drama about love and tradition.",
     director=johar,
-    poster_url="/public/pics/kkg.jpg",
+    poster_url="/pics/kkg.jpg",
     genres=[drama, romance],
     actors=[shahrukh, kajol]
 )
@@ -107,62 +107,62 @@ db.add_all([sci_fi, drama, action, thriller, adventure, fantasy])
 db.commit()
 
 # Directors
-nolan = models.Director(name="Christopher Nolan", birth_year=1970, bio="Known for complex narratives.",poster_url="/public/pics/cn.jpg")
-spielberg = models.Director(name="Steven Spielberg", birth_year=1946, bio="Legendary filmmaker with diverse works.",poster_url="/public/pics/ss.jpg")
-cameron = models.Director(name="James Cameron", birth_year=1954, bio="Director of epic blockbusters.",poster_url="/public/pics/jc.jpg")
-jackson = models.Director(name="Peter Jackson", birth_year=1961, bio="Director of Lord of the Rings trilogy.",poster_url="/public/pics/pj.jpg")
-wachowski = models.Director(name="Lana Wachowski", birth_year=1965, bio="Co-directed The Matrix.",poster_url="/public/pics/lw.jpg")
+nolan = models.Director(name="Christopher Nolan", birth_year=1970, bio="Known for complex narratives.",poster_url="/pics/cn.jpg")
+spielberg = models.Director(name="Steven Spielberg", birth_year=1946, bio="Legendary filmmaker with diverse works.",poster_url="/pics/ss.jpg")
+cameron = models.Director(name="James Cameron", birth_year=1954, bio="Director of epic blockbusters.",poster_url="/pics/jc.jpg")
+jackson = models.Director(name="Peter Jackson", birth_year=1961, bio="Director of Lord of the Rings trilogy.",poster_url="/pics/pj.jpg")
+wachowski = models.Director(name="Lana Wachowski", birth_year=1965, bio="Co-directed The Matrix.",poster_url="/pics/lw.jpg")
 db.add_all([nolan, spielberg, cameron, jackson, wachowski])
 db.commit()
 
 # Actors
-leo = models.Actor(name="Leonardo DiCaprio", birth_year=1974, bio="Oscar-winning actor.",poster_url="/public/pics/ln.jpg")
-ellen = models.Actor(name="Elliot Page", birth_year=1987, bio="Actor known for Juno and Inception.",poster_url="/public/pics/el.jpg")
-tom_hanks = models.Actor(name="Tom Hanks", birth_year=1956, bio="Beloved actor in drama and comedy.",poster_url="/public/pics/tm.jpg")
-sam_worthington = models.Actor(name="Sam Worthington", birth_year=1976, bio="Actor known for Avatar.",poster_url="/public/pics/sam.jpg")
-elijah = models.Actor(name="Elijah Wood", birth_year=1981, bio="Actor known for Lord of the Rings.",poster_url="/public/pics/elj.jpg")
-keanu = models.Actor(name="Keanu Reeves", birth_year=1964, bio="Actor known for The Matrix and John Wick.",poster_url="/public/pics/kn.jpg")
+leo = models.Actor(name="Leonardo DiCaprio", birth_year=1974, bio="Oscar-winning actor.",poster_url="/pics/ln.jpg")
+ellen = models.Actor(name="Elliot Page", birth_year=1987, bio="Actor known for Juno and Inception.",poster_url="/pics/el.jpg")
+tom_hanks = models.Actor(name="Tom Hanks", birth_year=1956, bio="Beloved actor in drama and comedy.",poster_url="/pics/tm.jpg")
+sam_worthington = models.Actor(name="Sam Worthington", birth_year=1976, bio="Actor known for Avatar.",poster_url="/pics/sam.jpg")
+elijah = models.Actor(name="Elijah Wood", birth_year=1981, bio="Actor known for Lord of the Rings.",poster_url="/pics/elj.jpg")
+keanu = models.Actor(name="Keanu Reeves", birth_year=1964, bio="Actor known for The Matrix and John Wick.",poster_url="/pics/kn.jpg")
 db.add_all([leo, ellen, tom_hanks, sam_worthington, elijah, keanu])
 db.commit()
 
 # Movies (10 total)
-inception = models.Movie(title="Inception", release_year=2010, description="A mind-bending thriller about dreams.", director=nolan,poster_url="/public/public/pics/incp.jpg")
+inception = models.Movie(title="Inception", release_year=2010, description="A mind-bending thriller about dreams.", director=nolan,poster_url="/public/pics/incp.jpg")
 inception.genres.extend([sci_fi, thriller])
 inception.actors.extend([leo, ellen])
 
-saving_private_ryan = models.Movie(title="Saving Private Ryan", release_year=1998, description="WWII epic about a rescue mission.", director=spielberg,poster_url="/public/pics/savi.jpg")
+saving_private_ryan = models.Movie(title="Saving Private Ryan", release_year=1998, description="WWII epic about a rescue mission.", director=spielberg,poster_url="/pics/savi.jpg")
 saving_private_ryan.genres.extend([drama, action])
 saving_private_ryan.actors.extend([tom_hanks])
 
-avatar = models.Movie(title="Avatar", release_year=2009, description="Epic science fiction film set on Pandora.", director=cameron,poster_url="/public/pics/ava.jpg")
+avatar = models.Movie(title="Avatar", release_year=2009, description="Epic science fiction film set on Pandora.", director=cameron,poster_url="/pics/ava.jpg")
 avatar.genres.extend([sci_fi, adventure])
 avatar.actors.extend([sam_worthington])
 
-titanic = models.Movie(title="Titanic", release_year=1997, description="Romantic tragedy aboard the Titanic.", director=cameron,poster_url="/public/pics/tit.jpg")
+titanic = models.Movie(title="Titanic", release_year=1997, description="Romantic tragedy aboard the Titanic.", director=cameron,poster_url="/pics/tit.jpg")
 titanic.genres.extend([drama, romance := models.Genre(name="Romance")])
 titanic.actors.extend([leo])
 
-lotr_fellowship = models.Movie(title="The Lord of the Rings: The Fellowship of the Ring", release_year=2001, description="Epic fantasy adventure.", director=jackson,poster_url="/public/pics/lord.jpg")
+lotr_fellowship = models.Movie(title="The Lord of the Rings: The Fellowship of the Ring", release_year=2001, description="Epic fantasy adventure.", director=jackson,poster_url="/pics/lord.jpg")
 lotr_fellowship.genres.extend([fantasy, adventure])
 lotr_fellowship.actors.extend([elijah])
 
-lotr_two_towers = models.Movie(title="The Lord of the Rings: The Two Towers", release_year=2002, description="Second part of the epic trilogy.", director=jackson,poster_url="/public/pics/imgs.jpg")
+lotr_two_towers = models.Movie(title="The Lord of the Rings: The Two Towers", release_year=2002, description="Second part of the epic trilogy.", director=jackson,poster_url="/pics/imgs.jpg")
 lotr_two_towers.genres.extend([fantasy, adventure])
 lotr_two_towers.actors.extend([elijah])
 
-lotr_return_king = models.Movie(title="The Lord of the Rings: The Return of the King", release_year=2003, description="Final part of the trilogy.", director=jackson,poster_url="/public/pics/king.jpg")
+lotr_return_king = models.Movie(title="The Lord of the Rings: The Return of the King", release_year=2003, description="Final part of the trilogy.", director=jackson,poster_url="/pics/king.jpg")
 lotr_return_king.genres.extend([fantasy, adventure])
 lotr_return_king.actors.extend([elijah])
 
-matrix = models.Movie(title="The Matrix", release_year=1999, description="Sci-fi classic about simulated reality.", director=wachowski,poster_url="/public/pics/matrix.jpg")
+matrix = models.Movie(title="The Matrix", release_year=1999, description="Sci-fi classic about simulated reality.", director=wachowski,poster_url="/pics/matrix.jpg")
 matrix.genres.extend([sci_fi, action])
 matrix.actors.extend([keanu])
 
-interstellar = models.Movie(title="Interstellar", release_year=2014, description="Space exploration and time relativity.", director=nolan,poster_url="/public/pics/inter.jpg")
+interstellar = models.Movie(title="Interstellar", release_year=2014, description="Space exploration and time relativity.", director=nolan,poster_url="/pics/inter.jpg")
 interstellar.genres.extend([sci_fi, drama])
 interstellar.actors.extend([leo])
 
-jurassic_park = models.Movie(title="Jurassic Park", release_year=1993, description="Dinosaurs brought back to life.", director=spielberg,poster_url="/public/pics/jur.jpg")
+jurassic_park = models.Movie(title="Jurassic Park", release_year=1993, description="Dinosaurs brought back to life.", director=spielberg,poster_url="/pics/jur.jpg")
 jurassic_park.genres.extend([sci_fi, adventure])
 
 
@@ -172,7 +172,7 @@ baijan = models.Movie(
     release_year=2015,
     description="Comedy-drama about friendship and education.",
     director=models.Director(name="Kabir Khan", birth_year=1962, bio="Known for socially relevant films."),
-    poster_url="/public/pics/rangi.jpeg"
+    poster_url="/pics/rangi.jpeg"
 )
 
 
